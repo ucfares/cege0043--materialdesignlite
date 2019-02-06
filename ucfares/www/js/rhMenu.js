880cec6277df1dc4900a3f676d27500cd8f28e37
ucfares <ucfares@ucl.ac.uk>
2019-02-06 05:52:10 -0500

    ucl logo try 3

--- ucfares/www/js/rhMenu.js
+++ ucfares/www/js/rhMenu.js
@@ -2,5 +2,5 @@ function menuClicked(){
 	alert ("you clicked the menu");
 }
 function addUCLLogo(){
-	document.getElementsById("ucllogo").innerHTML="<img src='images/ucl.png'>"
+	document.getElementById("ucllogo").innerHTML="<img src= 'images/ucl.png'>"
 }
